5b6e2ba3f143b37728c9ef819aaab5cc7147903c
Fool <kent.basalan@jmc.edu.ph>
2026-04-17 22:37:51 +0800

    Add initial HTML files for homepage and about page with images and links
    - Created index.html for the homepage with links to The Odin Project and an about page.
    - Added images including The Odin Project logo and a cute dog.
    - Created about.html page with an image of the dog.

---
+++ fiveserver.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  remoteLogs: true, // enable remoteLogs
+  // remoteLogs: "yellow", // You can use this line instead of 'true' if you want yellow logs
+  injectCss: false, // disable injecting css
+  navigate: true, // enable auto-navigation
+};
